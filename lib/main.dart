ef18ad3f2a4d4b22c3d9c61a48e183f4aeb88b6a
Som Rawat <somrawat007@gmail.com>
2021-07-04 21:37:40 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/home_page.dart';
+import 'package:time_tracker_flutter_course/pages/home_page.dart';
+import 'package:time_tracker_flutter_course/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = "Som Rawat";
-
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (content) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
