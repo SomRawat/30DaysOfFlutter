43f9d33d8ad2e9c0abe5a51e920cfd634a7793c8
Som Rawat <somrawat007@gmail.com>
2021-07-07 13:27:52 +0530

    day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,19 @@
 // import 'dart:ui';
 
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:time_tracker_flutter_course/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -20,7 +28,7 @@ class LoginPage extends StatelessWidget {
           SizedBox(
             height: 20.0,
           ),
-          Text("Welcome",
+          Text("Welcome $name",
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
           SizedBox(
             height: 20.0,
@@ -35,6 +43,10 @@ class LoginPage extends StatelessWidget {
                     labelText: "Username",
                     hintText: "Enter Username",
                   ),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -46,13 +58,47 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   height: 40.0,
                 ),
-                ElevatedButton(
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                  onPressed: () {
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                )
+
+                InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
+                    )
+
+                    // ElevatedButton(
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    ), // )
               ],
             ),
           ),
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,4 +1,5 @@
 class MyRoutes {
+  // ignore: non_constant_identifier_names
   static String LoginRoute = "/login";
   static String homeRoute = "/home";
 }
