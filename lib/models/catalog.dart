8533acfe039178e6455723ac896d106d6b9bdfb1
Som Rawat <somrawat007@gmail.com>
2021-07-10 20:39:32 +0530

    Day 12 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: " iphone 12 pro",
+        desc: "Apple iphone 12 gen",
+        price: 999,
+        color: "#22505a",
+        image:
+            "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-r1.jpg"),
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -7,21 +20,10 @@ class Item {
   final String image;
 
   Item(
-      {this.id = "Null",
+      {this.id = 0,
       this.name = "Null",
       this.desc = "Null",
       this.price = 0,
       this.color = "NUll",
       this.image = "Null"});
 }
-
-final products = [
-  Item(
-      id: "Codepuri",
-      name: " iphone 12 pro",
-      desc: "Apple iphone 12 gen",
-      price: 999,
-      color: "#22505a",
-      image:
-          " https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-family-hero?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1604021663000"),
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/models/catalog.dart';
 import 'package:time_tracker_flutter_course/widgets/drawer.dart';
+import 'package:time_tracker_flutter_course/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "Som Rawat";
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Internship"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter with " + name),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: CatalogModel.items[index]);
+          },
         ),
       ),
       drawer: MyDrawer(),
